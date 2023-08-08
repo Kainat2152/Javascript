b7dbb8d7355d7f2545aa5dd9f83438600661cfa6
Kainat2152 <136108673+Kainat2152@users.noreply.github.com>
2023-08-07 22:47:43 -0700

    Add files via upload

---
+++ operator,var,dtype.js
@@ -0,0 +1,80 @@
+
+var x="hello";
+document.write(x);
+document.write("<br>");
+
+//variables
+
+//const :can not overwrite and can not declared variable again which is already declared in const.
+const a="good girl";
+//const a ="Boy";(error occur if we do this )
+g="well";
+document.write(g);
+document.write("<br>");
+
+
+//var 
+
+var b=35;  
+var b="good boy"; 
+document.write(b);
+document.write("<br>");
+document.write(typeof b); //tells datatype (string)
+document.write("<br>");
+
+// let 
+
+
+let al=35;  
+ al=107; 
+document.write(al);
+document.write("<br>");
+document.write(typeof al); //number 
+document.write("<br>");
+
+
+var obj=["html" , "Css" , "Js"];
+document.write(obj);
+document.write("<br>");
+document.write(typeof obj); //obj
+document.write("<br>");
+
+
+//opeartors 
+var x = 20;
+var y = 50;
+
+var val = x*y;   
+document.write(val);
+document.write("<br>");
+
+
+let kk = x+y;
+kk++;   //increment
+document.write(kk);
+document.write("<br>");
+
+
+var val = x**y;   //exponential
+document.write(val);
+document.write("<br>");
+
+
+var val = x/y;    //division
+document.write(val);
+document.write("<br>");
+
+
+var val = x-y;   
+val--;     //decrement
+document.write(val);
+document.write("<br>");
+
+
+var val = x==y;  // assignemnt
+document.write(val);
+document.write("<br>");
+
+
+
+
